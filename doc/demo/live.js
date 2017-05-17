84b695fa1f7336c2ce33bcf8543930393d7c01c5
halwu(吴浩麟) <halwu@tencent.com>
2017-05-17 20:02:57 +0800

    fix: use https

--- doc/demo/live.js
+++ doc/demo/live.js
@@ -6,7 +6,7 @@ export class Live extends PureComponent {
   render() {
     return (
       <Reflv
-        url="http://2000.liveplay.myqcloud.com/live/2000_feceb63d176711e6b91fa4dcbef5e35a.flv"
+        url="https://2000.liveplay.myqcloud.com/live/2000_feceb63d176711e6b91fa4dcbef5e35a.flv"
         type="flv"
         isLive
         cors
