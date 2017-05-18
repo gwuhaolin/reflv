df805649cf421cc1c521c544eb552433c761f87e
halwu(吴浩麟) <halwu@tencent.com>
2017-05-18 14:59:54 +0800

    feat: add same source HLS live video to compare with HTTP-FLV

--- doc/demo/hls.js
+++ doc/demo/hls.js
@@ -5,7 +5,7 @@ export class HLS extends PureComponent {
   render() {
     return (
       <video autoPlay style={{ width: '100%' }}>
-        <source src="http://2000.liveplay.myqcloud.com/live/2000_feceb63d176711e6b91fa4dcbef5e35a.m3u8"/>
+        <source src="https://2000.liveplay.myqcloud.com/live/2000_feceb63d176711e6b91fa4dcbef5e35a.m3u8"/>
       </video>
     )
   }
